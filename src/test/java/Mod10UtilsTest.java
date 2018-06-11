b6f822cf6afb55e2e36b3b3db85fd70557b138a0
hliang9795 <hliang9795@hotmail.com>
2018-06-10 21:59:08 -0400

    Update Mod10UtilsTest.java

--- src/test/java/Mod10UtilsTest.java
+++ src/test/java/Mod10UtilsTest.java
@@ -8,6 +8,6 @@ public class Mod10UtilsTest {
     // to do: parameterize this test case. Need valid card# with all 10 possible check sum digits.
     public void testMod10Append() {
         Assertions.assertEquals(  4500000083808367L, Mod10Utils.appendMod10(450000008380836L), "Mod10Append");
-        Assertions.assertEquals(  x4500000054202590L, Mod10Utils.appendMod10(450000005420259L), "Mod10Append");
+        Assertions.assertEquals(  4500000054202590L, Mod10Utils.appendMod10(450000005420259L), "Mod10Append");
     }
 }

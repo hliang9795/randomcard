d106695ab42f3ab6580d3f2d226bee25d64af331
henryliang <hliang9795@gmail.com>
2018-06-10 20:30:20 -0400

    First ommit for test out github

---
+++ src/RandomCard/java/com/cedar/randomcard/Mod10Utils.java
@@ -0,0 +1,34 @@
+package com.cedar.randomcard;
+
+public class Mod10Utils {
+    /**
+     * Calculate check digit for a long number x of y digits. Assumes check digit
+     * will be append as last digits of the new number
+     *
+     * @param numberString
+     * @return
+     */
+    public static long appendMod10(long x) {
+        boolean isDouble = true;
+        long n=x;
+        long sum=0;
+        long d;
+        long c1,c2;
+
+        while( n>0 ) {
+            d = n % 10;
+            n = n/10;
+            c1 = (d * (isDouble ? 2 : 1));
+            c2 = (c1 >= 10) ? c1-10 + c1/10 : c1;
+            sum += c2;
+            //System.out.println(x + ":" + " d:" + d + " c1:" + c1 + " c2:" + c2 + " isDouble:" + isDouble + " sum:" + sum);
+            isDouble = !isDouble;
+        }
+        //for(c1=0; (c1+sum)%10!=0; c1++);
+        c1 = sum % 10;
+        c1 = (c1==0) ? 0 : 10 - c1;
+        //System.out.println(" check digits:" + c1 + "\n");
+        return x*10 + c1;
+    }
+}
+
---
+++ src/test/java/Mod10UtilsTest.java
@@ -0,0 +1,13 @@
+package com.cedar.randomcard;
+
+import org.junit.jupiter.api.*;
+
+public class Mod10UtilsTest {
+    @Test
+    @Tag("Util")
+    // to do: parameterize this test case. Need valid card# with all 10 possible check sum digits.
+    public void testMod10Append() {
+        Assertions.assertEquals(  4500000083808367L, Mod10Utils.appendMod10(450000008380836L), "Mod10Append");
+        Assertions.assertEquals(  4500000054202590L, Mod10Utils.appendMod10(450000005420259L), "Mod10Append");
+    }
+}
